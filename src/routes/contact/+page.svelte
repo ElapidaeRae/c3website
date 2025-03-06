37c7ad85d175658898744b26135c11bffdf48d51
ElapidaeRae <reubennoble16@gmail.com>
2025-03-06 10:23:37 +0000

    Update contact section layout and remove Bluesky link

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,6 @@
 <div class="justify-center items-center flex flex-col p-8">
     <h1 class="text-4xl md:p-8 m-2">Contact Me</h1>
-    <p class="text-lg">If you'd like to get in touch with me, you can find me at:</p>
+    <h1 class="text-2xl">You can find me at:</h1>
     <table class="table-auto border-collapse border-0 m-2">
         <tbody>
             <tr>
@@ -11,10 +11,10 @@
                 <td class="px-4 py-2">Discord</td>
                 <td class="px-4 py-2"><span class="text-pink-500">e.rae</span></td>
             </tr>
-            <tr>
-                <td class="px-4 py-2">Bluesky</td>
-                <td class="px-4 py-2"><a href="https://bsky.app/profile/elapid.bsky.social" class="text-pink-500">elapid.bsky.social</a></td>
-            </tr>
+<!--            <tr>-->
+<!--                <td class="px-4 py-2">Bluesky</td>-->
+<!--                <td class="px-4 py-2"><a href="https://bsky.app/profile/elapid.bsky.social" class="text-pink-500">elapid.bsky.social</a></td>-->
+<!--            </tr>-->
 <!--            <tr>-->
 <!--                <td class="px-4 py-2">Your walls</td>-->
 <!--                <td class="px-4 py-2"><a href="192.168.0.1" class="text-pink-500">I know your IP address</a></td>-->
@@ -25,4 +25,5 @@
             </tr>
         </tbody>
     </table>
+
 </div>
