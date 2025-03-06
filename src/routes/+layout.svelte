fc287eb7b535a9b1b207f3712923d7f293639b43
ElapidaeRae <reubennoble16@gmail.com>
2025-03-06 10:22:57 +0000

    Refactor layout to improve mobile navigation and add floppy disk icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,20 @@
 	let { children } = $props();
 </script>
 
-<main class="md:px-80 px-10 md:py-20 py-10 bg-gray-900 min-h-screen text-white">
-    <nav class="flex flex-nowrap md:flex-wrap justify-between items-center bg-gray-800 rounded-lg p-4">
-        <a href="/" class="text-white text-2xl p-2">elapidae.gay</a>
-        <div>
+<main class="lg:px-80 px-10 lg:py-20 py-10 bg-gray-900 min-h-screen text-white">
+    <div class="flex md:flex-nowrap flex-wrap justify-between items-center bg-gray-800 rounded-lg p-4 overscroll-auto">
+        <div class="flex justify-center items-center">
+            <img src="/src/lib/floppytransgif.gif" alt="Floppy Disk" class="md:h-16 h-8 mx-auto" />
+            <a href="/" class="text-white text-2xl p-2">elapidae.gay</a>
+        </div>
+        <nav class="flex justify-center items-center">
             <div class="px-4 space-x-4">
                 <a href="/" class="text-white">Home</a>
                 <a href="/contact" class="text-white">Contact</a>
-                <a href="/RaeNoble-CV.pdf" class="text-white">CV</a>
+<!--                <a href="/RaeNoble-CV.pdf" class="text-white">CV</a>-->
             </div>
-        </div>
-    </nav>
+        </nav>
+    </div>
 
     {@render children()}
 
