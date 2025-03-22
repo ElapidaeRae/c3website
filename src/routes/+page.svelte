d9b157f0b53c6d84c8dff720d253d4c067fff9cd
ElapidaeRae <reubennoble16@gmail.com>
2025-03-22 21:57:17 +0000

    Update introduction text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <div class="justify-center items-center flex flex-col p-8">
     <h1 class="text-4xl p-8">Hello!</h1>
     <p class="text-lg m-2">I'm the proprietor of this humble website, <span class="text-pink-500">ElapidaeRae</span>.</p>
-    <p class="text-lg m-2">I'm a programmer who dabbles in whatever catches my unending, insatiable curiosity.</p>
-    <p class="text-lg m-2">This website will serve as a place for me to share my thoughts, projects, and whatever else I feel like sharing.</p>
+    <p class="text-lg m-2">I'm a programmer who dabbles in whatever catches my insatiable curiosity.</p>
+    <p class="text-lg m-2">This website will serve as a place for me to share whatever I feel like sharing.</p>
+    <p class="text-sm text-gray-800 m-2">bit empty though innit?</p>
 </div>
