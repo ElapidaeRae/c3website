6cdbfd89e5dc4e6dcb9514828dcbbfb77953e3e0
ElapidaeRae <reubennoble16@gmail.com>
2025-03-22 22:13:37 +0000

    Remove alt attribute from floppy disk image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+    import image from '$lib/transdisc.gif'
 </script>
 
 <main class="lg:px-80 px-10 lg:py-20 py-10 bg-gray-900 min-h-screen text-white">
     <div class="flex md:flex-nowrap flex-wrap justify-between items-center bg-gray-800 rounded-lg p-4 overscroll-auto">
-        <div class="flex justify-center items-center">
-            <img src="/src/lib/floppytransgif.gif" alt="Floppy Disk" class="md:h-16 h-8 mx-auto" />
-            <a href="/" class="text-white text-2xl p-2">elapidae.gay</a>
-        </div>
+        <a href="/" class="text-white text-2xl p-2 flex justify-center items-center">
+            <img src={image} alt="" class="md:h-16 h-8 pr-2 mx-auto" />
+            elapidae.gay
+        </a>
         <nav class="flex justify-center items-center">
             <div class="px-4 space-x-4">
                 <a href="/" class="text-white">Home</a>
