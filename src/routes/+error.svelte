ab4947ccc71f685dcbb576c4fff7d4e0e629d8ea
ElapidaeRae <reubennoble16@gmail.com>
2025-05-12 13:00:44 +0100

    Refactor error page link structure and update button styles in random number generator

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,7 +6,8 @@
 <div class="flex flex-col items-center justify-center">
     <h1 class="text-4xl p-4 pt-12">Error {page.status}</h1>
     <p class="text-lg">{page.error.message}</p>
-    <div class="rounded-lg p-2 bg-purple-500 text-white mt-4">
-        <a href="/">Go back to the home page</a>
-    </div>
+    <a href="/">
+        <div class="rounded-lg p-2 bg-purple-500 text-white mt-4">Go back to the home page</div>
+    </a>
+
 </div>
--- src/routes/rng/+page.svelte
+++ src/routes/rng/+page.svelte
@@ -20,11 +20,11 @@
     <h1 class="p-2 text-4xl">Random Number Generator</h1>
     <p class="p-2 text-2xl">Random number: {num}</p>
     <div class="flex flex-row">
-        <button class="p-2 bg-blue-500 text-white m-2" on:click={() => random(100)}>Random 0-100</button>
+        <button class="p-2 bg-blue-500 text-white m-2 rounded-l-lg" on:click={() => random(100)}>Random 0-100</button>
         <button class="p-2 bg-blue-500 text-white m-2" on:click={() => randomRange(1,6)}>d6</button>
         <button class="p-2 bg-blue-500 text-white m-2" on:click={() => randomRange(1,8)}>d8</button>
         <button class="p-2 bg-blue-500 text-white m-2" on:click={() => randomRange(1,10)}>d10</button>
-        <button class="p-2 bg-blue-500 text-white m-2" on:click={() => randomRange(1,20)}>d20</button>
+        <button class="p-2 bg-blue-500 text-white m-2 rounded-r-lg" on:click={() => randomRange(1,20)}>d20</button>
     </div>
 </div>
 
